695b9a23d5cc339cb4e1128de519f47b3bb73894
RogerFC17 <rogerfreitascabral2010@gmail.com>
2026-03-25 11:37:20 +0000

    Finalização de Objetos JS

---
+++ objetos/obj-01.js
@@ -0,0 +1,14 @@
+//Objetos são estruturas chave-valor
+//Também chamadas "key value"
+//Objetos podem conter: arrays, string,
+//number, objects, functions...
+//possuem métodos próprios
+
+const jogador = {
+    nome:"Modric",
+    gols: 89,
+    times: ['Real Madrid'],
+}
+console.log(jogador.nome)
+console.log(jogador.gols)
+console.log(jogador.times[1])
---
+++ objetos/obj-02.js
@@ -0,0 +1,16 @@
+//A partir do Objeto de exemplo crie:
+//Um objeto com o tema à sua escola
+//Deve possuir no mínimo 6 propriedades
+//Imprima de um jeito amigável:
+//Ex.: console.log("Cor do veículo: " + carro.cor)
+
+const escola = {
+    quadradefutsal1: "16m x 27m",
+    quadradefutsal2: "20m x 40m",
+    quadradevolei: "18m x 9m",
+    saladeaula: "35 m² a 50 m²",
+}
+console.log("Medidas da primeira quadra de futsal: " + escola.quadradefutsal1)
+console.log("Medidas da segunda quadra de futsa: " + escola.quadradefutsal2)
+console.log("Medidas da quadra de vôlei: " + escola.quadradevolei)
+console.log("Medidas da sala de aula: " + escola.saladeaula)
